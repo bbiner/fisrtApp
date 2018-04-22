62f4b3dc568f7f2bee0769bba8a62764e011792c
bbiner <biner@91this.com>
2018-04-22 21:09:36 +0800

    2018.4.22

---
+++ js/pages/PopularPage.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    Image,
+    View,
+    TextInput,
+    ListView,
+    RefreshControl
+} from 'react-native';
+import ScrollableTabView, {ScrollableTabBar} from 'react-native-scrollable-tab-view';
+import NavigationBar from '../common/NavigationBar';
+import DataRepository from '../expand/dao/DataRepository';
+import RepositoryCell from '../common/RepositoryCell'
+const URL = 'https://api.github.com/search/repositories?q=';
+const QUERY_STR='&sort=stars';
+export default class PopularPage extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <View style={styles.container}>
+                <NavigationBar
+                title={'最热'}
+                style={{backgroundColor:'#2196F3'}}
+                />
+                <ScrollableTabView
+                    tabBarBackgroundColor={"#2196F3"}
+                    tabBarInactiveTextColor={"mintcream"}
+                    tabBarActiveTextColor={"white"}
+                    tabBarUnderLineStyle={{backgroundColor:'#e7e7e7',height:2}}
+                renderTabBar={()=><ScrollableTabBar/>}
+                >
+                    <PopularTab tabLabel={"Java"}>JAVA</PopularTab>
+                    <PopularTab tabLabel={"IOS"}>IOS</PopularTab>
+                    <PopularTab tabLabel={"Android"}>android</PopularTab>
+                    <PopularTab tabLabel={"JavaScript"}>js</PopularTab>
+                </ScrollableTabView>
+            </View>
+        )
+    }
+}
+
+class PopularTab extends Component{
+    constructor(props){
+        super(props);
+        this.dataRespository = new DataRepository();
+        this.state = {
+            result: '',
+            dataSource: new ListView.DataSource({rowHasChanged:(r1,r2)=>r1!==r2}),
+            isLoading: false
+        }
+    }
+    componentDidMount(){
+        this.loadData()
+    }
+    loadData(){
+        this.setState({
+           isLoading:true
+        });
+        let url = this.getUrl(this.text);
+        this.dataRespository.fetchNetRepository(url)
+            .then(result=>{
+                console.log(result.items);
+                this.setState({
+                    // result:result.items
+                    dataSource:this.state.dataSource.cloneWithRows(result.items),
+                    isLoading: false
+                })
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    getUrl(key){
+        return URL+this.props.tabLabel+QUERY_STR;
+    }
+    renderRow(rowData){
+        return (
+            <RepositoryCell rowData={rowData}/>
+        )
+    }
+    render(){
+        return (
+            <View style={{flex:1}}>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={(rowData) => this.renderRow(rowData)}
+                    refreshControl={
+                        <RefreshControl
+                        refreshing={this.state.isLoading}
+                        onRefresh={()=> this.loadData()}
+                        colors={['#2196F3']}
+                        tintColor={'#2196F3'}
+                        title={'Loading...'}
+                        titleColor={'#2196F3'}
+                        />
+                    }
+                >
+                </ListView>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    tips: {
+        fontSize: 29
+    }
+});
--- js/pages/WelcomePage.js
+++ js/pages/WelcomePage.js
@@ -1,35 +1,44 @@
+/**
+ * Created by penn on 2016/12/14.
+ */
+
 import React, {Component} from 'react';
 import {
     View,
     StyleSheet,
-    Text
-} from 'react-native';
-import Navigator from 'react-native-deprecated-custom-components';
-import HomePage from './HomePage';
+    Text,
+} from 'react-native'
+import NavigationBar from '../common/NavigationBar'
+import HomePage from './HomePage'
 export default class WelcomePage extends Component {
-    componentDidMount(){
-        this.timer = setTimeout(()=>{
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.timer=setTimeout(()=> {
             this.props.navigator.resetTo({
-                component:HomePage
-            })
-        }, 2000)
+                component: HomePage,
+            });
+        }, 2000);
     }
     componentWillUnmount(){
-        this.timer&&clearTimeout(this.timer)
+        this.timer&&clearTimeout(this.timer);
     }
-    render(){
+    render() {
         return (
-            <View>
-                <Text style={styles.content}>
-                    欢迎
-                </Text>
-            </View>
-        )
+            <View style={styles.container}>
+                <Text style={styles.tips}>欢迎</Text>
+            </View>)
     }
 }
-
 const styles = StyleSheet.create({
-    content: {
-        flex: 1
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems:'center',
+    },
+    tips: {
+        fontSize: 10,
     }
-});
+})
